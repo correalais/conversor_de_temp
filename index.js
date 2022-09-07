d5cf2dedb80c782cf6cc6e03eb56f83c446f5f81
Lais Correa <laiscorreamartins@gmail.com>
2022-09-07 14:12:39 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var btn = document.querySelector('.btn-converter');
+    var limpar = document.querySelector('.btn-limpar')
+    btn.onclick = function () {
+        var conversorC = document.querySelector('#celsius').value;
+        var conversorF = document.querySelector('#fahrenheit').value;
+        if (conversorC){
+            var tempConvertida =  (conversorC * 1.8) + 32;
+            document.querySelector('#fahrenheit').value = tempConvertida.toFixed(2);
+        } else {
+            var tempConvertida = (conversorF-32)/1.8;
+            document.querySelector('#celsius').value = tempConvertida.toFixed(2);
+        }
+    }
+
+    limpar.onclick = function () {
+        document.querySelector('#celsius').value = '';
+        document.querySelector('#fahrenheit').value = '';
+    }
+  });
+
+
+
