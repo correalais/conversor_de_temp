2e8e1538a866edf5a20662b849995d1570df22b0
Lais Correa <laiscorreamartins@gmail.com>
2022-09-07 16:19:31 -0300

    mudança nome das variaveis

--- js/index.js
+++ js/index.js
@@ -1,21 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(){
-    var btn = document.querySelector('.btn-converter');
-    var limpar = document.querySelector('.btn-limpar')
-    btn.onclick = function () {
-        var conversorC = document.querySelector('#celsius').value;
-        var conversorF = document.querySelector('#fahrenheit').value;
-        if (conversorC){
-            var tempConvertida =  (conversorC * 1.8) + 32;
+    document.querySelector('.botao-conversor').onclick = function () {
+        var celsius = document.querySelector('#celsius').value;
+        var fahrenheit = document.querySelector('#fahrenheit').value;
+        if (celsius){
+            var tempConvertida =  (celsius* 1.8) + 32;
             document.querySelector('#fahrenheit').value = tempConvertida.toFixed(2);
-        } else if (conversorF) {
-            var tempConvertida = (conversorF-32)/1.8;
+        } else if (fahrenheit) {
+            var tempConvertida = (fahrenheit-32)/1.8;
             document.querySelector('#celsius').value = tempConvertida.toFixed(2);
         } else {
             alert ("Você precisa preencher uma das temperaturas!")
         }
     }
 
-    limpar.onclick = function () {
+    document.querySelector('.botao-limpar').onclick = function () {
         document.querySelector('#celsius').value = '';
         document.querySelector('#fahrenheit').value = '';
     }
